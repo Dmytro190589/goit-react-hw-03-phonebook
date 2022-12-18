9a3f861523a0ba4356e31bf442d422a3b4086718
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-12-18 11:46:55 +0200

    clone

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import css from './App.module.css';
+import ContactForm from './ContactsForm/ContactsForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactsList/ContactsList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  filteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const register = filter.toLowerCase();
+    return contacts.filter(i => i.name.toLowerCase().includes(register));
+  };
+
+  deleteItem = itemId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(item => item.id !== itemId),
+    }));
+  };
+
+  filter = data => {
+    this.setState({ filter: data.currentTarget.value });
+  };
+
+  addContact = ({ name, number, id }) => {
+    this.state.contacts.some(e => e.name === name)
+      ? alert(`${name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [{ name, number, id }, ...prevState.contacts],
+        }));
+  };
+
+  render() {
+    return (
+      <div className={css.form}>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.filter} />
+        <ContactList
+          contacts={this.filteredContacts()}
+          onClick={this.deleteItem}
+        />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
+export default function ContactList({ contacts, onClick }) {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li className={css.list} key={id} id={id}>
+          <p>
+            {name} : {number}
+          </p>
+          <button className={css.btn} onClick={() => onClick(id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
